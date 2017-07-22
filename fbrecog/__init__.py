d922b010949d79b6337d4ec3bde13fb2608c6412
Sambhav Kothari <sambhavs.email@gmail.com>
2017-07-22 18:28:16 +0530

    Add error handling and fix code

--- fbrecog/__init__.py
+++ fbrecog/__init__.py
@@ -3,54 +3,80 @@ import requests
 import json
 
 
-def recognize(path,access_token,cookies,fb_dtsg):
-	""" 
-	Face recognition using Facebook's recognize method
-		Args:
-			path : file path of the photo to be processed
-			access_token : Facebook user access token
-			cookies : Facebook cookies
-			fb_dtsg : special Facebook token required for face recognition
-		Returns:
-			arr : array of recognitions with the name of recognized people and the certainity of each recognition
-	"""
+class FBRecog(object):
 
-	URL = "https://www.facebook.com/photos/tagging/recognition/?dpr=1"
-	
-	graph = GraphAPI(access_token)
-	
-	#Uploading the picture to Facebook	
-	post_id = graph.post( path = 'me/photos', source = open(path, 'rb'))['id']
-	headers = {
-			'x_fb_background_state': '1',
-			'origin': 'https://www.facebook.com',
-			'accept-encoding': 'gzip, deflate, lzma',
-			'accept-language': 'en-US,en;q=0.8',
-			'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2723.2 Safari/537.36',			'content-type': 'application/x-www-form-urlencoded',
-			'accept': '*/*',
-			'referer': 'https://www.facebook.com/',
-			'cookie': cookies,
-			'dnt': '1',
+    API_URL = "https://www.facebook.com/photos/tagging/recognition/?dpr=1"
 
-	}
-	
-	arr = []
-	payload = ""
-	
-	#Since the POST sometimes returns a blank array, retrying until a payload is obtained
-	while not payload:
-		data = 'recognition_project=composer_facerec&photos[0]='+post_id+'&target&is_page=false&include_unrecognized_faceboxes=false&include_face_crop_src=true&include_recognized_user_profile_picture=true&include_low_confidence_recognitions=true&__a=1&fb_dtsg='+fb_dtsg
-		req = requests.post(URL,data = data,headers=headers)
-		payload =  json.loads(req.text.replace('for (;;);',''))['payload']
-		if payload:
-			break
-	
-	for recog in payload[0]['faceboxes']:
-		name = recog['recognitions']
-		if name:
-			arr.append({'name':name[0]['user']['name'] , 'certainity' : name[0]['certainty']})
-	
-	#Deleting the uploaded picture
-	graph.delete(path = post_id)
+    def __init__(self, access_token, cookies, fb_dtsg):
+        self.access_token = access_token
+        self.cookies = cookies
+        self.fb_dtsg = fb_dtsg
+        self.headers = {'x_fb_background_state': '1',
+                        'origin': 'https://www.facebook.com',
+                        'accept-encoding': 'gzip, deflate, lzma',
+                        'accept-language': 'en-US,en;q=0.8',
+                        'user-agent': 'FBRecog/API',
+                        'content-type': 'application/x-www-form-urlencoded',
+                        'accept': '*/*',
+                        'referer': 'https://www.facebook.com/',
+                        'cookie': self.cookies,
+                        'dnt': '1'}
+        self.graph = GraphAPI(self.access_token)
 
-	return arr
+    def _post_photo(self, path):
+        try:
+            # Uploading the picture to Facebook
+            response = self.graph.post(path='me/photos', retry=3, source=open(path, 'rb'))
+        except Exception as e:
+            print(e)
+            return -1
+        else:
+            return response['id']
+
+    def _query_recognition_api(self, post_id):
+        result = []
+        payload = ""
+        data = 'recognition_project=composer_facerec&photos[0]=' + post_id
+        data += '&target&is_page=false&include_unrecognized_faceboxes=false&include_face_crop_src=true'
+        data += '&include_recognized_user_profile_picture=true&include_low_confidence_recognitions=true'
+        data += '&__a=1&fb_dtsg=' + self.fb_dtsg
+
+        # Since the POST sometimes returns a blank array, retrying until a payload is obtained
+        for i in range(20):
+            response = requests.post(self.API_URL, data=data, headers=self.headers)
+            payload = json.loads(response.text.replace('for (;;);', ''))['payload']
+            if "faceboxes" in payload and payload['faceboxes']:
+                break
+
+        print(payload)
+        for recog in payload[0]['faceboxes']:
+            name = recog['recognitions']
+            if name:
+                result.append({'name': name[0]['user']['name'], 'certainity': name[0]['certainty']})
+        return result
+
+    def recognize(self, path):
+        """Face recognition using Facebook's recognize method
+            Args:
+                path : file path of the photo to be processed
+            Returns:
+                result : array of recognitions with the name of recognized people
+                         and the certainity of each recognition
+        """
+        post_id = self._post_photo(path)
+        result = []
+        if post_id != -1:
+            try:
+                result = self._query_recognition_api(post_id)
+            except (KeyError, IndexError) as e:
+                print("Unable to fetch details. API unresponsive. Please try again later.")
+            except Exception as e:
+                print(e)
+
+        # Deleting the uploaded picture
+        print("Please wait. Cleaning up...")
+        self.graph.delete(path=post_id, retry=5)
+
+        print("Finished.")
+
+        return result
